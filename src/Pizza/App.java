1dd9f9974420f7b50df27181dbaea95f12bf1392
Your Name <you@example.com>
2024-11-26 13:37:50 +0300

    2f

---
+++ src/Pizza/App.java
@@ -0,0 +1,30 @@
+package Pizza;
+
+public class App {
+    public static void main(String[] args) {
+
+
+        Order fuy = new Order(1, "готовится");
+        Order fweuy = new Order(2, "Доставляется");
+        Order fut5y = new Order(3, "на складе");
+        Order fhkuy = new Order(4, "Готовится");
+        Order fu35y = new Order(5, "на складе");
+        Order fu23y = new Order(6, "Доставляется");
+
+        Queue.addOrder(fuy);
+        Queue.addOrder(fweuy);
+        Queue.addOrder(fut5y);
+        Queue.addOrder(fhkuy);
+        Queue.addOrder(fu35y);
+        Queue.addOrder(fu23y);
+
+
+
+
+
+
+
+
+
+    }
+}
