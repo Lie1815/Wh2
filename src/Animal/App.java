54d88ac1415b60b8b48106936b1c3725e2eb5f55
Your Name <you@example.com>
2024-12-17 13:15:43 +0300

    17/12

--- src/Animal/App.java
+++ src/Animal/App.java
@@ -3,12 +3,33 @@ package Animal;
 public class App {
     public static void main(String[] args) {
         Cat kuza = new Cat("kuza", 10.0);
+        Dog Gogi = new Dog("gogi", 15.0);
+        Spider Valera = new Spider("valera", 0.100);
+        Fish qwe = new Fish("qwe", 0.50);
+
         kuza.voice();
         kuza.sleep();
         kuza.catchMouse();
         System.out.println("котик ест");
-        kuza.eat();
-        System.out.println("котик ест");
+
+        System.out.println("--------------------------------------------------------------------------------");
+
+        Gogi.voice();
+        Gogi.sleep();
+        System.out.println("песик хомячит");
+
+        System.out.println("--------------------------------------------------------------------------------");
+
+        Valera.voice();
+        Valera.sleep();
+        System.out.println("паучок хомячит");
+
+        System.out.println("--------------------------------------------------------------------------------");
+
+        qwe.voice();
+        qwe.sleep();
+        System.out.println("рыбка хомячит");
+
 
 
     }
