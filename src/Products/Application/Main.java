32660e5043be35b74b092ca13be71d977ba79ae2
Your Name <you@example.com>
2024-12-20 13:42:34 +0300

    20/12

--- src/Products/Application/Main.java
+++ src/Products/Application/Main.java
@@ -3,27 +3,32 @@ package Products.Application;
 import Products.classes.MyProduct;
 import Products.classes.Product;
 
+import java.util.Scanner;
+
+
 public class Main {
+
     public static void main(String[] args) {
+        MyProduct myProduct = new MyProduct(100, 70, 200, 2000);
+        Scanner scanner = new Scanner(System.in);
 
-        Product bread = new Product("хлеб", 9,3, 49, 265);
-        Product chiken = new Product("курица", 31, 3.6, 0, 165);
-        Product chocolate = new Product("шоколад", 5, 500, 100, 546);
-        Product apple = new Product("яблоко", 7, 2, 69 , 121);
-        Product cucumber = new Product("огурец", 7, 8, 12, 92);
-        Product soda = new Product("газировка", 3, 151,  24,   638);
-
-        MyProduct myProduct = new MyProduct(50 , 50, 300, 1000);
-
-        myProduct.addProduct(apple);
-        myProduct.addProduct(chiken);
-        myProduct.addProduct(chocolate);
-        myProduct.addProduct(cucumber);
-        myProduct.addProduct(soda);
-
-        myProduct.printList();
+        String name = null;
+        for (int i = 0; i < 6; i++) {
+            System.out.print("Введите название продукта: ");
+            name = scanner.nextLine();
+            System.out.print("Введите количество белков: ");
 
 
+            double proteins = scanner.nextDouble();
+            System.out.print("Введите количество жиров: ");
+            double fats = scanner.nextDouble();
+            System.out.print("Введите количество углеводов: ");
+            double carbohydrates = scanner.nextDouble();
+            scanner.nextLine();
 
+            myProduct.addProduct(name, proteins, fats, carbohydrates);
+        }
+        myProduct.displayProducts();
+        scanner.close();
     }
 }
--- src/Products/classes/MyProduct.java
+++ src/Products/classes/MyProduct.java
@@ -1,37 +1,60 @@
 package Products.classes;
 
+
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 
 public class MyProduct {
-    private double maxprotein;
-    private double maxfats;
-    private double maxcarbohydrates;
-    private double maxcalories;
-    private List<Product> products;
 
-    public MyProduct(double maxcalories, double maxcarbohydrates, double maxfats, double maxprotein) {
+        private double maxProteins;
+        private double maxFats;
+        private double maxCarbohydrates;
+        private double maxCalories;
+        private List<Product> productList;
 
-        this.maxcalories = maxcalories;
-        this.maxcarbohydrates = maxcarbohydrates;
-        this.maxprotein = maxprotein;
-        this.maxfats = maxfats;
-        this.products = new ArrayList<>();
-    }
-
-    public void addProduct(Product product){
-        if (product.isValue(maxprotein, maxfats, maxcarbohydrates, maxcalories)){
-            System.out.println("продукт " + product.getName() + "   добавлен.");
-        }else{
-            System.out.println("Продукт " + product.getName() + " не может быть добавлен " + " слишком большое содержание калорий " +
-                    "");
+        public MyProduct(double maxProteins, double maxFats, double maxCarbohydrates, double maxCalories) {
+            this.maxProteins = maxProteins;
+            this.maxFats = maxFats;
+            this.maxCarbohydrates = maxCarbohydrates;
+            this.maxCalories = maxCalories;
+            this.productList = new ArrayList<>();
         }
-    }
 
-    public void printList(){
-        System.out.println("список продуктов: ");
-        for (Product product : products){
-            System.out.println("- " + product.getName());
+        public void addProduct(String name, double proteins, double fats, double carbohydrates) {
+            double calories = Product.calculateCalories(proteins, fats, carbohydrates);
+
+            if (validateProduct(proteins, fats, carbohydrates, calories)) {
+                productList.add(new Product(name, proteins, fats, carbohydrates));
+                System.out.println("Продукт добавлен: " + name);
+            } else {
+                System.out.println("Продукт не может быть добавлен: " + name);
+                if (proteins > maxProteins) {
+                    System.out.println("Слишком большое содержание белков.");
+                }
+                if (fats > maxFats) {
+                    System.out.println("Слишком большое содержание жиров.");
+                }
+                if (carbohydrates > maxCarbohydrates) {
+                    System.out.println("Слишком большое содержание углеводов.");
+                }
+                if (calories > maxCalories) {
+                    System.out.println("Слишком большое содержание калорий.");
+                }
+            }
         }
-    }
-}
+
+        private boolean validateProduct(double proteins, double fats, double carbohydrates, double calories) {
+            return proteins <= maxProteins && fats <= maxFats && carbohydrates <= maxCarbohydrates && calories <= maxCalories;
+        }
+
+        public void displayProducts() {
+            System.out.println("Список добавленных продуктов:");
+            for (Product product : productList) {
+                System.out.println(product);
+            }
+        }
+
+        }
